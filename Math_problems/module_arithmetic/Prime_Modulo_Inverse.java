9f8907c0762dd5f39ba834906afaeccd76bf7d7e
JavaCoderSubham <subham.gupta@motivitylabs.com>
2023-09-19 22:17:47 +0530

    Prime_Modulo_Inverse New Solutions.

--- Math_problems/module_arithmetic/Prime_Modulo_Inverse.java
+++ Math_problems/module_arithmetic/Prime_Modulo_Inverse.java
@@ -45,6 +45,29 @@
  *
  * * Explanation With Solution *
  *
+ * ************ Iterative *************
+ *
+ *     long power(long x, long y, long p) {
+ *         long res = 1;        // Initialize result
+ *         x = x % p;        // Update x if it is more than or equal to p
+ *         while (y > 0) {
+ *             // If y is odd, multiply x with result
+ *             if ((y & 1) == (long)1)
+ *                 res = (res*x) % p;
+ *             y = y >> 1;
+ *             x = (x * x) % p;
+ *         }
+ *         return res;
+ *     }
+ *
+ *     public int solve(int A, int B) {
+ *         // Modular inverse of A Modulo B = pow(A, B - 2, B)
+ *         return (int)power(A, B - 2, B);
+ *     }
+ *
+ *
+ * ************ Recursion *************
+ *
  *  	public int solve(int A, int B) {
  * 		if (A == 0)
  * 			return 0;
@@ -71,7 +94,7 @@
  */
 
 
-package Basic_Math_problems;
+package Math_problems.module_arithmetic;
 
 public class Prime_Modulo_Inverse {
 
