c8bd5c72f494d4b62eb8a1f2ac2d12ba81ef9441
JavaCoderSubham <subham.gupta@motivitylabs.com>
2023-09-29 22:38:10 +0530

    Prime_finder_with_3 solution.

---
+++ Array/Prime_finder_with_3.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * Problem Description
+ * in the small town of Numeric, there was a legendary math club called "the prime
+ * detectives." this club consisted of a group of brilliant young students who loved
+ * solving mathematical mysteries.
+ *
+ * one day, a mysterious message arrived at the club's headquarters.
+ * The message contained two numbers, A and B, nad a cryptic challenge stated that
+ * within the range of numbers from A to B, There were special prime numbers that
+ * held the key to unlocking a hidden treasure. special prime numbers are the
+ * prime number that end with 3.
+ *
+ *
+ * Given A and B can You find All prime number in the range whose last digit is 3.
+ *
+ * problem Constraints
+ * 1 <= A, B <= 10^5
+ *
+ *
+ */
+
+
+package Array;
+
+public class Prime_finder_with_3 {
+
+    public static void main(String[] args) {
+
+    }
+}
