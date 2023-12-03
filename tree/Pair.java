21bc42f37da85fec4ca98c4a274820684a020450
JavaCoderSubham <subham.gupta@motivitylabs.com>
2023-12-03 19:30:10 +0530

    Pair

---
+++ tree/Pair.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Pair {
+
+    TreeNode node;
+    Integer count;
+
+    public Pair(TreeNode node, Integer count) {
+        this.node = node;
+        this.count = count;
+    }
+
+}
